9a41661992221ac6703bef53926e9b878fd0c24a
biswo <biswo.dev98@gmail.com>
2025-04-19 22:52:44 +0530

    initial commit

---
+++ src/test/java/com/spring/ai/intro/SpringAiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
